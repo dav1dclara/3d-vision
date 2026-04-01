7b7e44ebca37e2c2fad5bab0b4103308ca0409cf
oscipal <oscipal@student-cluster1.inf.ethz.ch>
2026-04-01 15:22:02 +0200

    poisson meshing

--- scripts/view_mesh.py
+++ scripts/view_mesh.py
@@ -4,17 +4,43 @@ import open3d as o3d
 
 rr.init("mesh_viewer", spawn=True)
 
-mesh = o3d.io.read_triangle_mesh("reconstruction.ply")
-mesh.compute_vertex_normals()
+nksr = o3d.io.read_triangle_mesh("outputs/nksr_reconstruction.ply")
+nksr.compute_vertex_normals()
 
-vertices = np.asarray(mesh.vertices)
-faces    = np.asarray(mesh.triangles)
-normals  = np.asarray(mesh.vertex_normals)
+nksr_vertices = np.asarray(nksr.vertices)
+nksr_faces    = np.asarray(nksr.triangles)
+nksr_normals  = np.asarray(nksr.vertex_normals)
 
-rr.log("mesh", rr.Mesh3D(
-    vertex_positions=vertices,
-    triangle_indices=faces,
-    vertex_normals=normals,
+poisson = o3d.io.read_triangle_mesh("outputs/possoin_reconstruction.ply")
+poisson.compute_vertex_normals()
+
+poisson_vertices = np.asarray(poisson.vertices)
+poisson_faces    = np.asarray(poisson.triangles)
+poisson_normals  = np.asarray(poisson.vertex_normals)
+
+pcd = o3d.io.read_triangle_mesh("outputs/pcd_reconstruction.ply")
+pcd.compute_vertex_normals()
+
+pcd_vertices = np.asarray(pcd.vertices)
+pcd_faces    = np.asarray(pcd.triangles)
+pcd_normals  = np.asarray(pcd.vertex_normals)
+
+rr.log("nksr", rr.Mesh3D(
+    vertex_positions=nksr_vertices,
+    triangle_indices=nksr_faces,
+    vertex_normals=nksr_normals,
+))
+
+rr.log("pcd", rr.Mesh3D(
+    vertex_positions=pcd_vertices,
+    triangle_indices=pcd_faces,
+    vertex_normals=pcd_normals,
+))
+
+rr.log("poisson", rr.Mesh3D(
+    vertex_positions=poisson_vertices,
+    triangle_indices=poisson_faces,
+    vertex_normals=poisson_normals,
 ))
 
 input("Press Enter to exit...")
